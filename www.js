ba5baa9676f1e3bd0a8a2f6f408f21c5ba9f51ea
javier.nogales <jnogales@bilbomatica.es>
2020-12-15 14:34:03 +0100

    NodeJS basic boot

---
+++ www.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Create server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+// Listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+    console.log(`Server runnign at http://${hostname}:${port}/`);
+});
