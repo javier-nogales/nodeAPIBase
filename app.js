75bbd30064dac214cd68bba09dd07fdecd5529f0
javier.nogales <jnogales@bilbomatica.es>
2020-12-15 15:29:08 +0100

    NodeJS basic boot with ExpressJS framework

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express');
+const app = express();
+const port = 3000
 
-// Create server
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+app.get('/', (req, res) => {
+    res.send('Hellow World');
 });
 
-// Listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-    console.log(`Server runnign at http://${hostname}:${port}/`);
+app.listen(port, () => {
+    console.log(`Example app listening at http://lodalhost:${port}/`);
 });
-
-
